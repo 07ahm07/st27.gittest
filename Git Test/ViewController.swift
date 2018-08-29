c1d1213fe8e4d28a409969046180aca91749ceff
Ahmet TAŞKIN <ahmettaskin@me-tas.com>
2018-08-29 15:13:40 +0300

    Initial Commit

---
+++ Git Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Git Test
+//
+//  Created by Ahmet TAŞKIN on 29.08.2018.
+//  Copyright © 2018 Ahmet TAŞKIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
